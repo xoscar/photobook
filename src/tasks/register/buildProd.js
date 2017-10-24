7d5cdebe03b3ed38718fd40b2f0f0b3b49c5578d
Reyes Gaucin, Oscar R <oscar.r.reyes.gaucin@accenture.com>
2017-10-23 22:38:16 -0500

    :zap: added contact delete modal for the front end and unit test for the backend

--- src/tasks/register/buildProd.js
+++ src/tasks/register/buildProd.js
@@ -19,11 +19,10 @@ module.exports = function(grunt) {
   grunt.registerTask('buildProd', [
     'compileAssets',
     'concat',
-    'uglify',
     'cssmin',
     'linkAssetsBuildProd',
     'clean:build',
-    'copy:build'
+    'copy:build',
   ]);
 };
 
